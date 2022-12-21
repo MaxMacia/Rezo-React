8381c4fcfb6df0965272996075d490da835fe9b0
Maxence Macia <maxence.macia@gmail.com>
2022-12-21 17:23:13 +0100

    mise en place de la connection avec mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
+
+async function mongooseConnect() {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGO_SECRET}.mongodb.net/?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        console.log("Connexion à MongoDB réussie!")
+    } catch (error) { console.log("Connexion à MongoDB à échoué!") };
+};
+mongooseConnect();
 
 app.use(express.json());
 
@@ -10,5 +26,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+
 
 module.exports = app;

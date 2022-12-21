3ef9e33fe6e46cc3b60e04d37d16619112a2137b
Maxence Macia <maxence.macia@gmail.com>
2022-12-21 19:05:05 +0100

    mise en place de la requête post /api/auth/login

--- backend/controllers/rezoUser.js
+++ backend/controllers/rezoUser.js
@@ -12,5 +12,30 @@ exports.signup = async (req, res, next) => {
         await rezoUser.save();
         const message = "Utilisateur enregistré";
         res.status(201).json(message);
-    } catch (error) { res.status(500).json({ error }) };
+    } catch(error) { res.status(500).json(error.message) };
+};
+
+exports.login = async (req, res, next) => {
+    try {
+        const rezoUser = await RezoUser.findOne({ identifier: req.body.identifier });
+        const message ="Paire identifiant/mot de passe invalide";
+        if (rezoUser === null) {
+            throw error = new Error(message)
+        } else {
+            const valid = await bcrypt.compare(req.body.password, rezoUser.password);
+            if (!valid) {
+                throw error = new Error(message)
+            } else { res.status(200).json({
+                userId: rezoUser._id,
+                token: 'TOKEN'
+            }) }
+        }
+    } catch(error) {
+        const message ="Paire identifiant/mot de passe invalide";
+        if (error.message === message) {
+            res.status(401).json(error.message)
+        } else {
+            res.status(500).json(error.message)
+        }
+    };
 };
--- backend/routes/rezoUser.js
+++ backend/routes/rezoUser.js
@@ -4,5 +4,6 @@ const rezoUserCtrl = require('../controllers/rezoUser');
 const router = express.Router();
 
 router.post('/signup', rezoUserCtrl.signup);
+router.post('/login', rezoUserCtrl.login)
 
 module.exports = router;
